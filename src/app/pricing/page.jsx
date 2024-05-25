daf687a5570c1f18bcb6bd3d352d21052e1c8750
Mark Steven Alba <140490597+markStevenAlba@users.noreply.github.com>
2024-05-25 13:10:02 +0800

    pricing

--- src/app/pricing/page.jsx
+++ src/app/pricing/page.jsx
@@ -1,8 +1,31 @@
+import styles from './pricing.module.css'
+
+
 const PricingPage = () => {
     return (
-      <div>
-          PricingPage
+      <div className="">
+      <div className={styles.title}>
+        <h2>
+        Available Plans:
+        </h2>
       </div>
+      <div className={styles.price_container} > 
+        <button className={styles.price_button}>
+          <h1>$5.00</h1>
+          <br></br>
+          <p>1 month validity</p>
+          <p>No Customization</p>
+          <p>No Dashboard</p>
+        </button>
+        <button className={styles.price_button} >
+        <h1>$85.00</h1>
+          <br></br>
+          <p>1 month validity</p>
+          <p>App Customization</p>
+          <p>Personal Dashboard</p>
+        </button>
+      </div>
+    </div>
     )
   }
   
